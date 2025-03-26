8d643e685ad6e2e4bf0ee4d5ac52d27697a4b627
jakwro199 <wroniastyj19@gmail.com>
2025-03-26 01:46:25 +0100

    Update HanoiRhymer.java

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,22 +1,18 @@
 package edu.kis.vh.nursery;
 
-// 4.1 poprawa nazw klas
-// 4.2 zmiana konwencji nazewniczej metod
-
 public class HanoiRhymer extends defaultCountingOutRhymer {
-	// 5.1 Poprawienie błędów w  konwencji nazewniczej pól klas we wszystkich klasach
 
-
-	int totalRejected = 0;
+    private int totalRejected = 0;
 
     public int reportRejected() {
         return totalRejected;
     }
 
+    @Override
     public void countIn(int in) {
-	    if (!callCheck() && in > peekaboo())
-	            totalRejected++;
-		else
-	            super.countIn(in);
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
     }
 }
